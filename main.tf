7d29fec120b9e6b349f788a99d7b5ab65a3a68c8
Cristian Beraha <cristian@cloudops.works>
2025-07-30 14:26:55 -0300

    Add monitoring group support with YAML-configured observability settings and Terraform CloudWatch alarms. Update required Terraform version to `>=1.7`.

--- main.tf
+++ main.tf
@@ -8,305 +8,93 @@
 #
 
 locals {
-  slo_in = try(var.settings.service_level_objectives, [])
-  slo_set_env = [
-    for slo in local.slo_in : merge(slo,
-      length(try(slo.service_level_indicator.eks, {})) > 0 ? {
-        service_level_indicator = merge(slo.service_level_indicator, {
-          environment = format("eks:%s/%s", slo.service_level_indicator.eks.cluster_name, slo.service_level_indicator.eks.namespace)
-          name        = try(slo.service_level_indicator.eks.name, slo.service_level_indicator.name)
-          type        = try(slo.service_level_indicator.eks.type, "Service")
-        })
-      } : {},
-      length(try(slo.service_level_indicator.lambda, {})) > 0 ? {
-        service_level_indicator = merge(slo.service_level_indicator, {
-          environment = format("lambda:%s", slo.service_level_indicator.lambda.function_name)
-          name        = try(slo.service_level_indicator.lambda.function_name, slo.service_level_indicator.name)
-          type        = try(slo.service_level_indicator.lambda.type, "Service")
-        })
-      } : {},
-    )
+  configs    = yamldecode(file("${path.module}/observability-config.yaml"))
+  config_map = { for conf in local.configs : conf.name => conf }
+  groups = [
+    for group in var.monitor_groups : {
+      monitor_name = format("[P%s][%s][%s][%s] %s %s",
+        group.priority,
+        lower(var.org.organization_unit),
+        lower(var.org.environment_name),
+        lower(var.org.environment_type),
+        group.name,
+        group.service_name,
+      )
+      group  = group
+      config = local.config_map[group.target_name]
+    }
   ]
-  slo_operational = flatten([
-    for slo in local.slo_set_env : [
-      for operation in slo.service_level_indicator.operations : {
-        name        = format("%s %s op", try(slo.name, slo.service_level_indicator.name), replace(operation, "/[\\/\\$\\%\\^]/", "-"))
-        description = try(slo.description, "SLO Setting for ${try(slo.name, slo.service_level_indicator.name)} - ${operation}")
-        sli = {
-          comparison_operator = try(slo.service_level_indicator.comparisson, "LessThan")
-          metric_threshold    = try(slo.service_level_indicator.threshold, null)
-          sli_metric = {
-            key_attributes = {
-              Environment = slo.service_level_indicator.environment
-              Name        = slo.service_level_indicator.name
-              Type        = slo.service_level_indicator.type
-            }
-            metric_type    = try(slo.service_level_indicator.metric_type, "LATENCY")
-            operation_name = operation
-            period_seconds = try(slo.service_level_indicator.period_seconds, 60)
-            statistic      = try(slo.service_level_indicator.statistic, "p99")
-          }
-        }
-        goal = {
-          attainment_goal = try(slo.goal.attainment, 99.9)
-          interval = {
-            rolling_interval = {
-              duration      = try(slo.goal.duration, 7)
-              duration_unit = try(slo.goal.duration_unit, "DAY")
-            }
-          }
-          warning_threshold = try(slo.goal.warning_threshold, 80)
-        }
-        tags = try(slo.tags, {})
-      }
-    ] if try(slo.enabled, true) && slo.type == "operational"
-  ])
-  # Golden Signals SLOs - (Latency, Traffic, Errors, Saturation)
-  slo_golden_signals = flatten([
-    for slo in local.slo_set_env : [
-      # Latency signal
-      {
-        name        = format("gs-latency-%s", lower(try(slo.name, slo.service_level_indicator.name)))
-        description = try(slo.description, "[Golden Signals] [Latency] SLO for ${try(slo.name, slo.service_level_indicator.name)}")
-        sli = {
-          comparison_operator = try(slo.service_level_indicator.comparisson, "LessThan")
-          metric_threshold    = slo.service_level_indicator.latency_threshold
-          sli_metric = {
-            metric_data_queries = [
-              {
-                account_id = try(slo.service_level_indicator.account_id, null)
-                id         = "latencyQuery1"
-                metric_stat = {
-                  metric = {
-                    namespace   = "ApplicationSignals"
-                    metric_name = "Latency"
-                    dimensions = [
-                      {
-                        name  = "Environment"
-                        value = slo.service_level_indicator.environment
-                      },
-                      {
-                        name  = "Service"
-                        value = slo.service_level_indicator.name
-                      }
-                    ]
-                  }
-                  period = try(slo.service_level_indicator.period_seconds, 300)
-                  stat   = try(slo.service_level_indicator.statistic, "Average")
-                }
-                return_data = true
-              }
-            ]
-          }
-        }
-        goal = {
-          attainment_goal = try(slo.goal.attainment, 99.9)
-          interval = {
-            rolling_interval = {
-              duration      = try(slo.goal.duration, 7)
-              duration_unit = try(slo.goal.duration_unit, "DAY")
-            }
-          }
-          warning_threshold = try(slo.goal.warning_threshold, 80)
-        }
-        tags = try(slo.tags, {})
-      },
-      # Errors Signal
-      {
-        name        = format("gs-errors-%s", lower(try(slo.name, slo.service_level_indicator.name)))
-        description = try(slo.description, "[Golden Signals] [Errors] SLO for ${try(slo.name, slo.service_level_indicator.name)}")
-        request_based_sli = {
-          request_based_sli_metric = {
-            monitored_request_count_metric = {
-              bad_count_metric = [
-                {
-                  account_id = try(slo.service_level_indicator.account_id, null)
-                  id         = "badCount1"
-                  metric_stat = {
-                    metric = {
-                      namespace   = "ApplicationSignals"
-                      metric_name = "Error"
-                      dimensions = [
-                        {
-                          name  = "Environment"
-                          value = slo.service_level_indicator.environment
-                        },
-                        {
-                          name  = "Service"
-                          value = slo.service_level_indicator.name
-                        }
-                      ]
-                    }
-                    period = try(slo.service_level_indicator.period_seconds, 300)
-                    stat   = "Average"
-                  }
-                  return_data = true
-                }
-              ]
-            }
-            total_request_count_metric = [
-              {
-                account_id = try(slo.service_level_indicator.account_id, null)
-                id         = "totalCount1"
-                metric_stat = {
-                  metric = {
-                    namespace   = "ApplicationSignals"
-                    metric_name = "Latency"
-                    dimensions = [
-                      {
-                        name  = "Environment"
-                        value = slo.service_level_indicator.environment
-                      },
-                      {
-                        name  = "Service"
-                        value = slo.service_level_indicator.name
-                      }
-                    ]
-                  }
-                  period = try(slo.service_level_indicator.period_seconds, 300)
-                  stat   = "SampleCount"
-                }
-                return_data = true
-              }
-            ]
-          }
-        }
-        goal = {
-          attainment_goal = try(slo.goal.attainment, 99.9)
-          interval = {
-            rolling_interval = {
-              duration      = try(slo.goal.duration, 7)
-              duration_unit = try(slo.goal.duration_unit, "DAY")
-            }
-          }
-        }
-        tags = try(slo.tags, {})
-      },
-      # Traffic Signal
-      {
-        name        = format("gs-traffic-%s", lower(try(slo.name, slo.service_level_indicator.name)))
-        description = try(slo.description, "[Golden Signals] [Traffic] SLO for ${try(slo.name, slo.service_level_indicator.name)}")
-        sli = {
-          comparison_operator = try(slo.service_level_indicator.comparisson, "LessThanOrEqualTo")
-          metric_threshold    = slo.service_level_indicator.traffic_threshold
-          sli_metric = {
-            metric_data_queries = [
-              {
-                account_id = try(slo.service_level_indicator.account_id, null)
-                id         = "trafficQuery1"
-                metric_stat = {
-                  metric = {
-                    namespace   = "ApplicationSignals"
-                    metric_name = "Latency"
-                    dimensions = [
-                      {
-                        name  = "Environment"
-                        value = slo.service_level_indicator.environment
-                      },
-                      {
-                        name  = "Service"
-                        value = slo.service_level_indicator.name
-                      }
-                    ]
-                  }
-                  period = try(slo.service_level_indicator.period_seconds, 300)
-                  stat   = "SampleCount"
-                }
-                return_data = true
-              }
-            ]
-          }
-        }
-        goal = {
-          attainment_goal = try(slo.goal.attainment, 99.9)
-          interval = {
-            rolling_interval = {
-              duration      = try(slo.goal.duration, 7)
-              duration_unit = try(slo.goal.duration_unit, "DAY")
-            }
-          }
-          warning_threshold = try(slo.goal.warning_threshold, 80)
-        }
-        tags = try(slo.tags, {})
-      },
-      # Saturation Signal
-      {
-        name        = format("gs-saturation-%s", lower(try(slo.name, slo.service_level_indicator.name)))
-        description = try(slo.description, "[Golden Signals] [Saturation] SLO for ${try(slo.name, slo.service_level_indicator.name)}")
-        sli = {
-          comparison_operator = try(slo.service_level_indicator.comparisson, "LessThan")
-          metric_threshold    = slo.service_level_indicator.saturation_threshold
-          sli_metric = {
-            metric_data_queries = [
-              {
-                account_id = try(slo.service_level_indicator.account_id, null)
-                id         = "saturationPercentage"
-                expression = "100 * (saturationQuery1)"
-                return_data = true
-              },
-              {
-                account_id = try(slo.service_level_indicator.account_id, null)
-                id         = "saturationQuery1"
-                metric_stat = {
-                  metric = {
-                    namespace   = "ContainerInsights"
-                    metric_name = upper(slo.service_level_indicator.saturation_metric) == "CPU" ? "pod_cpu_utilization_over_pod_limit" : "pod_memory_utilization_over_pod_limit"
-                    dimensions = [
-                      {
-                        name  = "ClusterName"
-                        value = slo.service_level_indicator.eks.cluster_name
-                      },
-                      {
-                        name  = "Namespace"
-                        value = slo.service_level_indicator.eks.namespace
-                      },
-                      {
-                        name  = "Service"
-                        value = slo.service_level_indicator.eks.name
-                      }
-                    ]
-                  }
-                  period = try(slo.service_level_indicator.period_seconds, 300)
-                  stat   = "Average"
-                }
-                return_data = false
-              }
-            ]
-          }
-        }
-        goal = {
-          attainment_goal = try(slo.goal.attainment, 99.9)
-          interval = {
-            rolling_interval = {
-              duration      = try(slo.goal.duration, 7)
-              duration_unit = try(slo.goal.duration_unit, "DAY")
-            }
-          }
-          warning_threshold = try(slo.goal.warning_threshold, 80)
-        }
-        tags = try(slo.tags, {})
-      }
-    ] if try(slo.enabled, true) && slo.type == "golden-signal"
-  ])
-
-  slo_all = concat(local.slo_operational, local.slo_golden_signals, [])
+  groups_map = {
+    for group in local.groups : group.group.target_name => group
+  }
+  sns_actions    = [for item in data.aws_sns_topic.sns : item.arn]
+  lambda_actions = [for item in data.aws_lambda_function.lambda : item.arn]
+  alarm_actions  = concat(local.sns_actions, local.lambda_actions)
+  ok_actions     = concat(local.sns_actions, local.lambda_actions)
 }
 
-resource "awscc_applicationsignals_service_level_objective" "slo" {
-  for_each = {
-    for slo in local.slo_all : slo.name => slo
-  }
-  name                     = each.value.name
-  description              = each.value.description
-  sli                      = try(each.value.sli, null)
-  goal                     = try(each.value.goal, null)
-  request_based_sli        = try(each.value.request_based_sli, null)
-  burn_rate_configurations = try(each.value.burn_rate_configurations, null)
-  exclusion_windows        = try(each.value.exclusion_windows, null)
-  tags = toset([
-    for k, v in merge(local.all_tags, each.value.tags) : {
-      key   = k
-      value = v
+resource "aws_cloudwatch_metric_alarm" "monitor" {
+  for_each                              = local.groups_map
+  alarm_name                            = each.value.monitor_name
+  alarm_description                     = templatestring(each.value.config.description, each.value.group)
+  comparison_operator                   = each.value.config.comparison_operator
+  evaluation_periods                    = each.value.config.evaluation_periods
+  datapoints_to_alarm                   = each.value.config.datapoints_to_alarm
+  period                                = each.value.config.period
+  metric_name                           = try(each.value.config.metric_name, null)
+  statistic                             = try(each.value.config.statistic, null)
+  namespace                             = try(each.value.config.namespace, null)
+  threshold                             = try(each.value.group.threshold, each.value.config.default_threshold, null)
+  threshold_metric_id                   = try(each.value.config.threshold_metric_id, null)
+  unit                                  = try(each.value.config.unit, null)
+  treat_missing_data                    = try(each.value.config.treat_missing_data, "missing")
+  evaluate_low_sample_count_percentiles = try(each.value.config.evaluate_low_sample, null)
+  actions_enabled                       = true
+  alarm_actions                         = local.alarm_actions
+  ok_actions                            = local.ok_actions
+  dimensions = length(try(each.value.config.dimensions, {})) > 0 ? {
+    for dim_key, dim_template in each.value.config.dimensions :
+    dim_key => templatestring(dim_template, each.value.group)
+  } : null
+  dynamic "metric_query" {
+    for_each = try(each.value.config.metric_query, {})
+    content {
+      id          = metric_query.key
+      account_id  = try(each.value.group.account_id, null)
+      expression  = try(metric_query.value.expression, null)
+      label       = try(metric_query.value.label, null)
+      return_data = try(metric_query.value.return_data, null)
+      period      = try(metric_query.value.period, null)
+      dynamic "metric" {
+        for_each = length(try(metric_query.value.metric, {})) > 0 ? [1] : []
+        content {
+          metric_name = metric_query.value.metric.metric_name
+          namespace   = metric_query.value.metric.namespace
+          period      = metric_query.value.metric.period
+          stat        = metric_query.value.metric.statistic
+          unit        = try(metric_query.value.metric.unit, null)
+          dimensions = length(try(metric_query.value.metric.dimensions, {})) > 0 ? {
+            for dim_key, dim_template in metric_query.value.metric.dimensions :
+            dim_key => templatestring(dim_template, each.value.group)
+          } : null
+        }
+      }
     }
-  ])
+  }
+}
+
+data "aws_sns_topic" "sns" {
+  for_each = {
+    for target in var.alarm_targets : target.name => target
+    if target.type == "sns"
+  }
+  name = each.value.name
+}
+
+data "aws_lambda_function" "lambda" {
+  for_each = {
+    for target in var.alarm_targets : target.name => target
+    if target.type == "lambda"
+  }
+  function_name = each.value.name
 }
