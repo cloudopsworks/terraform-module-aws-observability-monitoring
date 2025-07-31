0e4765fb239b0d4bacac7249d19b5ec05396a7cb
Cristian Beraha <cristian@cloudops.works>
2025-07-31 12:17:36 -0300

    Refactor observability configurations: unify `group` and `monitor` definitions, update templatestring logic, adjust dimensions and thresholds for consistency across EKS and Lambda alarms.

--- main.tf
+++ main.tf
@@ -10,20 +10,24 @@
 locals {
   configs    = yamldecode(file("${path.module}/observability-config.yaml"))
   config_map = { for conf in local.configs : conf.name => conf }
-  groups = [
-    for group in var.monitor_groups : {
-      monitor_name = format("[P%s] [%s] [%s] [%s] %s - %s",
-        group.priority,
+  groups = flatten([
+    for group in var.monitor_groups : [
+      for monitor in group.monitors: {
+      monitor_name = format("[P%s] [%s] [%s] [%s] %s - %s - %s",
+        monitor.priority,
         lower(var.org.organization_unit),
         lower(var.org.environment_name),
         group.service_name,
-        group.name,
+        monitor.name,
+        group.type,
         lower(var.org.environment_type)
       )
       group  = group
+      monitor = monitor
       config = local.config_map[group.target_name]
     }
   ]
+  ])
   groups_map = {
     for group in local.groups : group.group.target_name => group
   }
@@ -36,15 +40,15 @@ locals {
 resource "aws_cloudwatch_metric_alarm" "monitor" {
   for_each                              = local.groups_map
   alarm_name                            = each.value.monitor_name
-  alarm_description                     = templatestring(each.value.config.description, each.value.group)
+  alarm_description                     = templatestring(each.value.config.description, each.value)
   comparison_operator                   = each.value.config.comparison_operator
-  evaluation_periods                    = try(each.value.group.evaluation_periods, each.value.config.evaluation_periods)
-  datapoints_to_alarm                   = try(each.value.group.datapoints_to_alarm, each.value.config.datapoints_to_alarm)
-  period                                = length(try(each.value.config.metric_query, {})) == 0 ? try(each.value.group.period, each.value.config.period, null) : null
+  evaluation_periods                    = try(each.value.monitor.evaluation_periods, each.value.config.evaluation_periods)
+  datapoints_to_alarm                   = try(each.value.monitor.datapoints_to_alarm, each.value.config.datapoints_to_alarm)
+  period                                = length(try(each.value.config.metric_query, {})) == 0 ? try(each.value.monitor.period, each.value.config.period, null) : null
   metric_name                           = try(each.value.config.metric_name, null)
   statistic                             = try(each.value.config.statistic, null)
   namespace                             = try(each.value.config.namespace, null)
-  threshold                             = try(each.value.group.threshold, each.value.config.default_threshold, null)
+  threshold                             = try(each.value.monitor.threshold, each.value.config.default_threshold, null)
   threshold_metric_id                   = try(each.value.config.threshold_metric_id, null)
   unit                                  = try(each.value.config.unit, null)
   treat_missing_data                    = try(each.value.config.treat_missing_data, "missing")
@@ -54,35 +58,35 @@ resource "aws_cloudwatch_metric_alarm" "monitor" {
   ok_actions                            = local.ok_actions
   dimensions = length(try(each.value.config.dimensions, {})) > 0 ? {
     for dim_key, dim_template in each.value.config.dimensions :
-    dim_key => templatestring(dim_template, each.value.group)
+    dim_key => templatestring(dim_template, each.value)
   } : null
   dynamic "metric_query" {
     for_each = try(each.value.config.metric_query, {})
     content {
       id          = metric_query.key
-      account_id  = try(each.value.group.account_id, null)
+      account_id  = try(each.value.monitor.account_id, null)
       expression  = try(metric_query.value.expression, null)
       label       = try(metric_query.value.label, null)
       return_data = try(metric_query.value.return_data, null)
-      period      = length(try(metric_query.value.metric, {})) == 0 ? try(each.value.group.period, metric_query.value.period, null) : null
+      period      = length(try(metric_query.value.metric, {})) == 0 ? try(each.value.monitor.period, metric_query.value.period, null) : null
       dynamic "metric" {
         for_each = length(try(metric_query.value.metric, {})) > 0 ? [1] : []
         content {
           metric_name = metric_query.value.metric.metric_name
           namespace   = metric_query.value.metric.namespace
-          period      = try(each.value.group.period, metric_query.value.metric.period)
+          period      = try(each.value.monitor.period, metric_query.value.metric.period)
           stat        = metric_query.value.metric.statistic
           unit        = try(metric_query.value.metric.unit, null)
           dimensions = length(try(metric_query.value.metric.dimensions, {})) > 0 ? {
             for dim_key, dim_template in metric_query.value.metric.dimensions :
-            dim_key => templatestring(dim_template, each.value.group)
+            dim_key => templatestring(dim_template, each.value)
           } : null
         }
       }
     }
   }
   tags = merge(local.all_tags, {
-    "alarm-priority"       = each.value.group.priority
+    "alarm-priority"       = each.value.monitor.priority
     "observability-config" = each.value.config.name
     "service-name"         = each.value.group.service_name
   })
